3883cd6dcc3ad2c83bf0939e2bbc26588fac88b1
Jayden Chapman <jaychapmancode@gmail.com>
2023-02-21 10:26:51 -0800

    finalized project

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -25,6 +25,22 @@ module.exports = () => {
         swSrc: './src-sw.js',
         swDest: 'src-sw.js',
       }),
+      new WebpackPwaManifest({
+        name: 'JATE',
+        short_name: 'JATE',
+        description: 'Keep track of everything written in this program!',
+        background_color: '#7eb4e2',
+        theme_color: '#7eb4e2',
+        start_url: './',
+        publicPath: './',
+        icons: [
+          {
+            src: path.resolve('src/images/logo.png'),
+            sizes: [96, 128, 192, 256, 384, 512],
+            destination: path.join('assets', 'icons'),
+          },
+        ],
+      })
     ],
 
     module: {
